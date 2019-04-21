7582f7821df9d000584c0a8e9ae761c61957c95b
Rosswell <rosswelltiongco@gmail.com>
2019-04-21 09:13:55 -0700

    can scrape via python (will take looong time eta 30 mis)

--- scraper.py
+++ scraper.py
@@ -1,9 +1,12 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import json
+import time
+
 
 professorRatings = {}
 
+startTime = time.time()
 
 def getRating(professorID):
     my_url = "http://www.ratemyprofessors.com/ShowRatings.jsp?tid={}".format(professorID)
@@ -17,15 +20,25 @@ def getRating(professorID):
     rating = containers.text
     return rating
 
-
+# Import file
 with open('ids.txt') as json_file:
     professors = json.load(json_file)
-    for professor in sorted(professors)[:5]:
-     # print("{}: {}".format(professor, professors[professor]))
-     print("Parsing:",professor)
-     professorRatings[professor] = getRating(professors[professor])
-    # for professor in professors:
-    # professorRatings[professor] = getRating(professors[professor])
 
+
+# parse through all 5,000 professors
+counter = 1
+total = len(professors)
+for professor in sorted(professors)[:50]:
+ # print("{}: {}".format(professor, professors[professor]))
+ timeSince = time.time() - startTime
+ print("Time: {}s | Progress: {}/{} ({})% -  Parsing: {}".format(str(timeSince)[:3],counter,total,str(counter/total)[:5],professor))
+ professorRatings[professor] = getRating(professors[professor])
+ counter+=1
+
+# Store to file
 with open('data.json', 'w') as outfile:
     json.dump(professorRatings, outfile)
+
+endTime = time.time()
+totalTime = endTime-startTime
+print("Took {} seconds".format(totalTime))
